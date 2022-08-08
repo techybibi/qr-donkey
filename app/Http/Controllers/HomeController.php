41be557ca360b589c9c0635c87f1a99f19482ee7
Bibith K Mathew <bibith.k@webandcrafts.in>
2022-08-08 12:21:34 +0530

    url conflict resolved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return Redirect::to('https://barakbusinesses.com');
+        return \redirect()->route('qr');
     }
 }
